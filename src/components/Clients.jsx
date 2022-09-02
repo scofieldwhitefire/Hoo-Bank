d4b3fafcd13b9730120da71d47b3721b5816ff1b
tetelainv <tetelainv@gmail.com>
2022-09-02 02:03:54 -0400

    Client Done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { clients } from '../constants'
+import style from '../style'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+const Clients = () => (
+  <section className={`${style.flexCenter} my-5`}>
+    <div className={`${style.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${style.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+          <img src={client.logo} alt='client' className='sm:w-[192px] w-[100px] object-contain' />
+        </div>
+        ))}
+    </div>
+  </section>
   )
-}
 
 export default Clients
